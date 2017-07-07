21dfefe2acd96ae59a9809126254b90c2e5bd0eb
AnbhuSavi <saviprabhu123@gmail.com>
2017-07-07 14:58:38 +0530

    Create Fibonacci29.java

---
+++ Fibonacci29.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.*;
+import java.lang.*;
+public class Fibonacci29 {
+ 
+    public static void main(String a[]){
+         
+         int fibCount = 15;
+         int[] fib = new int[fibCount];
+         fib[0] = 0;
+         fib[1] = 1;
+         for(int i=2; i < fibCount; i++){
+             fib[i] = fib[i-1] + fib[i-2];
+         }
+ 
+         for(int i=0; i< fibCount; i++){
+                 System.out.print(fib[i] + " ");
+         }
+    }
+}
