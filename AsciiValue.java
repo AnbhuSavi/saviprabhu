c8c839d5876bb41bc6709dc40932c804232eaed4
AnbhuSavi <saviprabhu123@gmail.com>
2017-07-07 14:00:13 +0530

    Create AsciiValue.java

---
+++ AsciiValue.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class AsciiValue 
+{
+    public static void main(String args[]) 
+    {
+        int i;
+        for(i = 0; i < 26; i++){
+            System.out.println((char)('A'+i) + " --> " + ('A'+i));
+             
+         }
+    }
+}
